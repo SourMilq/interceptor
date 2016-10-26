88ce0504d441cc0fded7260f801355ac9fcb934e
Wasiur Rahman <wrahman0@gmail.com>
2016-10-25 23:16:02 -0400

    Heroku config

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@
 var Sequelize = require('sequelize');
 
 module.exports = function (config) {
+    console.log("config.db ", config.db);
     return new Sequelize(config.db.database, config.db.user, config.db.password, {
         host: config.db.host,
         dialect: config.db.driver,
