dab52835d8d5016691380dc2b11b760b1648c001
Wasiur Rahman <wrahman0@gmail.com>
2016-10-26 00:46:01 -0400

    Trying postgres

--- config/db.js
+++ config/db.js
@@ -4,10 +4,19 @@ var Sequelize = require('sequelize');
 
 module.exports = function (config) {
     console.log("config.db ", config.db);
-    return new Sequelize(config.db.database, config.db.user, config.db.password, {
-        host: config.db.host,
-        dialect: config.db.driver,
-        port: config.db.port,
-        pool: {maxIdleTime: 120000}
-    });
+    if (process.env.HEROKU_POSTGRESQL_YELLOW_URL){
+        return new Sequelize(process.env.HEROKU_POSTGRESQL_YELLOW_URL, {
+            dialect: 'postgres',
+            protocol: 'postgres',
+            port: match[4],
+            host: match[3],
+            logging: false
+        });
+    }else {
+        return new Sequelize(config.db.database, config.db.user, config.db.password, {
+            host: config.db.host,
+            dialect: config.db.driver,
+            port: config.db.port
+        });
+    }
 };
