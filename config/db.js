b41c35e08ccd427d9ff238d9feaab5e7a896675b
Wasiur Rahman <wrahman0@gmail.com>
2016-10-26 01:26:13 -0400

    Hard coding

--- config/db.js
+++ config/db.js
@@ -4,14 +4,23 @@ var Sequelize = require('sequelize');
 
 module.exports = function (config) {
     console.log("config.db ", config.db);
-    return new Sequelize(config.db.database, config.db.user, config.db.password, {
-        host: config.db.host,
-        dialect: config.db.driver,
-        port: config.db.port,
-        pool: {
-            maxConnections: 10,
-            minConnections: 1,
-            maxIdleTime:    1000
-        }
-    });
+    if (process.env.HEROKU_POSTGRESQL_YELLOW_URL){
+        var match = process.env.HEROKU_POSTGRESQL_YELLOW_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+        return new Sequelize(match[5], match[1], match[2], {
+            dialect:  'postgres',
+            protocol: 'postgres',
+            port:     match[4],
+            host:     match[3],
+            logging: false,
+            dialectOptions: {
+                ssl: true
+            }
+        });
+    }else {
+        return new Sequelize(config.db.database, config.db.user, config.db.password, {
+            host: config.db.host,
+            dialect: config.db.driver,
+            port: config.db.port
+        });
+    }
 };
