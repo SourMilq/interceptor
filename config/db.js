e25c6a0c6de54136875208ae2ccc9e5814314bdc
Wasiur Rahman <wrahman0@gmail.com>
2016-10-26 01:19:20 -0400

    Hard coding

--- config/db.js
+++ config/db.js
@@ -7,6 +7,11 @@ module.exports = function (config) {
     return new Sequelize(config.db.database, config.db.user, config.db.password, {
         host: config.db.host,
         dialect: config.db.driver,
-        port: config.db.port
+        port: config.db.port,
+        pool: {
+            maxConnections: 10,
+            minConnections: 1,
+            maxIdleTime:    1000
+        }
     });
 };
